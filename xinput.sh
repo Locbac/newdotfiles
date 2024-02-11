466d2307ef6775f9e6534783057d38c5b42e51b3
Amon <martinhost003@gmail.com>
2024-02-11 15:48:17 -0500

    Revert "push"
    This reverts commit 5d945cc66d091aeb80658053a6e5f7c04d2db9e7.

--- xinput.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-xinput set-prop 11 "libinput Tapping Enabled" "1"
-xinput set-prop 11 "libinput Tapping Drag Enabled" "1"
-xinput set-prop 11 "libinput Natural Scrolling Enabled" "1"
-xinput set-prop 11 "libinput Scrolling Pixel Distance" "10"
-xinput set-prop 11 "libinput Accel Speed" "0.31"
-xinput set-prop 11 "libinput Middle Emulation Enabled" "1"
