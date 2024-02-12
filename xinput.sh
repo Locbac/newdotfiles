586f9c84968f3d3ab028042de9c8f0460b8057be
Amon <martinhost003@gmail.com>
2024-02-12 06:42:13 -0500

    push

--- xinput.sh
+++ xinput.sh
@@ -1,8 +1,10 @@
 #/bin/sh
 
-xinput set-prop 11 "libinput Tapping Enabled" "1"
-xinput set-prop 11 "libinput Tapping Drag Enabled" "1"
-xinput set-prop 11 "libinput Natural Scrolling Enabled" "1"
-xinput set-prop 11 "libinput Scrolling Pixel Distance" "10"
-xinput set-prop 11 "libinput Accel Speed" "0.31"
-xinput set-prop 11 "libinput Middle Emulation Enabled" "1"
+xinput set-prop 17 "libinput Tapping Enabled" "1"
+xinput set-prop 17 "libinput Tapping Drag Enabled" "1"
+xinput set-prop 17 "libinput Natural Scrolling Enabled" "1"
+xinput set-prop 17 "libinput Scrolling Pixel Distance" "10"
+xinput set-prop 17 "libinput Accel Speed" "0.31"
+xinput set-prop 17 "libinput Middle Emulation Enabled" "1"
+# xinput set-prop 24 "libinput Accel Profile Enabled" "0"
+xinput set-prop 24 "libinput Accel Speed" "-1"
