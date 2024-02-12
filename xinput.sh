9b4507e412c6e977c0ba4ced94282416b1ab5096
Amon <martinhost003@gmail.com>
2024-02-12 13:03:40 -0500

    push

--- xinput.sh
+++ xinput.sh
@@ -1,10 +1,11 @@
 #/bin/sh
-
-xinput set-prop 17 "libinput Tapping Enabled" "1"
-xinput set-prop 17 "libinput Tapping Drag Enabled" "1"
-xinput set-prop 17 "libinput Natural Scrolling Enabled" "1"
-xinput set-prop 17 "libinput Scrolling Pixel Distance" "10"
-xinput set-prop 17 "libinput Accel Speed" "0.31"
-xinput set-prop 17 "libinput Middle Emulation Enabled" "1"
+touchpad_id=$(xinput list | grep -i 'SYNA2393:00 06CB:7A13 Touchpad' | awk '{print $6}' | awk -F= '{print $2}')
+xinput set-prop $touchpad_id "libinput Tapping Enabled" "1"
+xinput set-prop $touchpad_id "libinput Tapping Drag Enabled" "1"
+xinput set-prop $touchpad_id "libinput Natural Scrolling Enabled" "1"
+xinput set-prop $touchpad_id "libinput Scrolling Pixel Distance" "10"
+xinput set-prop $touchpad_id "libinput Accel Speed" "0.31"
+xinput set-prop $touchpad_id "libinput Middle Emulation Enabled" "1"
 # xinput set-prop 24 "libinput Accel Profile Enabled" "0"
-xinput set-prop 24 "libinput Accel Speed" "-1"
+mxmaster_id=$(xinput list | grep -i -E 'mx master 3|logitech mx master 3' | awk '{print $6}' | awk -F= '{print $2}')
+xinput set-prop $mxmaster_id "libinput Accel Speed" "-1"
