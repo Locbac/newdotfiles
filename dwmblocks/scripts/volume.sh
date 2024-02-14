a97c6c2e4eec8355aaf80a964ae90d220f18c0d2
Amon <martinhost003@gmail.com>
2024-02-14 10:37:05 -0500

    push

--- dwmblocks/scripts/volume.sh
+++ dwmblocks/scripts/volume.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # amixer sget Master | tail -1 | awk '{print $5 }' | sed 's@\\(\\[\\|\\]\\)@@g'
 output=$(amixer sget Master)
-percentage=$(echo "$output" | awk -F'[][]' '/Playback/ {print $2}')
-
+# percentage=$(echo $(echo "$output" | awk -F'[][]' '/Playback/ {print $2}') | sed '1,2d;$d')
+percentage=$(amixer sget Master | awk -F'[][]' '/Playback/ { printf "%s", $2 }' | tr -d '\n')
 if [[ $output == *"[off]"* ]]; then
 	echo "Mute"
 else
