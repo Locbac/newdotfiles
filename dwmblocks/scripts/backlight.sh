d6cefe5817e2b7af6f0acc9e1b2d966713123f7f
Amon <martinhost003@gmail.com>
2024-02-14 10:56:22 -0500

    push

--- dwmblocks/scripts/backlight.sh
+++ dwmblocks/scripts/backlight.sh
@@ -1,3 +1,20 @@
-#/bin/bash
-output=$(cat /sys/class/backlight/intel_backlight/brightness); max_value=120000; percentage=$((output * 100 / max_value)); echo $percentage" }
-echo $output
+#!/bin/bash
+
+brightness_file="/sys/class/backlight/intel_backlight/brightness"
+max_brightness=12000
+
+read_brightness() {
+	cat "$brightness_file"
+}
+
+calculate_percentage() {
+	local brightness=$1
+	local percentage=$(awk "BEGIN { printf \"%.0f\", ($brightness / $max_brightness) * 10 }")
+	echo "$percentage"
+}
+
+current_brightness=$(read_brightness)
+current_percentage=$(calculate_percentage "$current_brightness")
+
+# echo "Current Brightness: $current_brightness"
+echo "$current_percentage%"
