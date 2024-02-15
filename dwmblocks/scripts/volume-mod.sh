bd93a4ac6a3d53435ef1676944bdf55c07a2af6c
Amon <martinhost003@gmail.com>
2024-02-14 19:56:57 -0500

    push

--- dwmblocks/scripts/volume-mod.sh
+++ dwmblocks/scripts/volume-mod.sh
@@ -2,11 +2,23 @@
 
 # Check if card 1 exists
 if amixer -c 1 info >/dev/null 2>&1; then
-	# Card 1 exists, modify 'Targus Audio'
-	amixer -c 1 sset 'Targus Audio' "$1"
+	# Card 1 exists
+	if [[ "$1" == "tog" ]]; then
+		# Toggle 'Targus Audio'
+		amixer -c 1 set 'Targus Audio' toggle
+	else
+		# Set volume level for 'Targus Audio'
+		amixer -c 1 sset 'Targus Audio' "$1"
+	fi
 	pkill -RTMIN+10 dwmblocks
 else
-	# Card 1 doesn't exist, modify 'Master' on card 0
-	amixer -c 0 sset Master "$1" unmute
+	# Card 1 doesn't exist
+	if [[ "$1" == "tog" ]]; then
+		# Toggle 'Master' on card 0
+		amixer -q set Master toggle
+	else
+		# Set volume level for 'Master' on card 0
+		amixer -c 0 sset Master "$1" unmute
+	fi
 	pkill -RTMIN+10 dwmblocks
 fi
