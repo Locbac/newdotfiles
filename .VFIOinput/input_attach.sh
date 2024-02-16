8b72f71144b23c663dc9d2f71cea2dced714a3ca
Amon <martinhost003@gmail.com>
2024-02-16 18:10:12 -0500

    push

---
+++ .VFIOinput/input_attach.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+virsh attach-device win11 $HOME/.VFIOinput/input_1.xml
+virsh attach-device win11 $HOME/.VFIOinput/input_2.xml
---
+++ .VFIOinput/input_detach.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+virsh detach-device win11 $HOME/.VFIOinput/input_1.xml
+virsh detach-device win11 $HOME/.VFIOinput/input_2.xml
