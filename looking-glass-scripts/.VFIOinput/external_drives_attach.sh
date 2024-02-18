f6733b992c8a8141b53e8cc2bbdaaef3b0c88155
Amon <martinhost003@gmail.com>
2024-02-18 11:09:47 -0500

    push

---
+++ looking-glass-scripts/.VFIOinput/external_drives_attach.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+virsh attach-device win11 $HOME/.VFIOinput/my-passport.xml
+virsh attach-device win11 $HOME/.VFIOinput/my-passport-slim.xml
---
+++ looking-glass-scripts/.VFIOinput/external_drives_detach.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+virsh detach-device win11 $HOME/.VFIOinput/my-passport.xml
+virsh detach-device win11 $HOME/.VFIOinput/my-passport-slim.xml
