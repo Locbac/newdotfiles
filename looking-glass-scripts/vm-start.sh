30bc0e3adb0f0c0d215922d1b7174e304a87ea3c
Amon <martinhost003@gmail.com>
2024-02-18 14:03:58 -0500

    push

--- looking-glass-scripts/vm-start.sh
+++ looking-glass-scripts/vm-start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # virsh start $1                                                      # Optional line, this will simply start the VM if it isn't running. You can delete this if you wish. Make sure to change the name to your VM name.
+# /bin/bash $HOME/.VFIOinput/external_drives_attach.sh & # Attaches external drives
+# /bin/bash $HOME/.VFIOinput/input_attach.sh
 /usr/local/bin/looking-glass-client -m 97 -c DXGI >/dev/null 2>&1 & # Starts Looking Glass, and ignores all output (We aren't watching anyways)
 # /usr/bin/sudo /usr/local/bin/scream -m /dev/shm/scream-ivshmem -o alsa & # Starts Scream
 
-wait -n     # We wait for any of these processes to exit. (Like closing the Looking Glass window, in our case)
+# wait -n     # We wait for any of these processes to exit. (Like closing the Looking Glass window, in our case)
+wait $!     # Waits for the most recent background process (looking-glass-client) to exit
 pkill -P $$ # We kill the remaining processes (In our case, scream)
